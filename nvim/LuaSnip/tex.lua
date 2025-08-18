0e1ed60337896272775a39b3f8dad138469e872e
Ahnaf Rafi <ahnaf.al.rafi@gmail.com>
2025-08-18 12:46:16 -0400

    Add neovim source files.

---
+++ nvim/LuaSnip/tex.lua
@@ -0,0 +1,360 @@
+require("luasnip.session.snippet_collection").clear_snippets("tex")
+
+local ls = require("luasnip")
+local t = ls.text_node
+local s = ls.snippet
+local i = ls.insert_node
+local f = ls.function_node
+local isn = ls.indent_snippet_node
+local sn = ls.snippet_node
+-- local c = ls.choice_node
+-- local d = ls.dynamic_node
+-- local r = ls.restore_node
+-- local events = require("luasnip.util.events")
+-- local ai = require("luasnip.nodes.absolute_indexer")
+-- local extras = require("luasnip.extras")
+-- local l = extras.lambda
+-- local rep = extras.rep
+-- local p = extras.partial
+-- local m = extras.match
+-- local n = extras.nonempty
+-- local dl = extras.dynamic_lambda
+-- local fmt = require("luasnip.extras.fmt").fmt
+-- local fmta = require("luasnip.extras.fmt").fmta
+-- local conds = require("luasnip.extras.expand_conditions")
+-- local postfix = require("luasnip.extras.postfix").postfix
+-- local types = require("luasnip.util.types")
+-- local parse = require("luasnip.util.parser").parse_snippet
+local ms = ls.multi_snippet
+-- local k = require("luasnip.nodes.key_indexer").new_key
+--
+
+return {
+  s(
+    {trig = "sum"},
+    {t("\\sum_{"), i(1, "i = 1"), t("}^{"), i(2, "n"), t("} ")}
+  ),
+
+  s(
+    {trig = "int"},
+    {t("\\int_{"), i(1, "a"), t("}^{"), i(2, "b"), t("} "), i(0),
+      t(" \\; \\mathrm{d} "), i(3, "x")}
+  ),
+
+  s(
+    {trig = "wh"},
+    {t("\\widehat{"), i(1), t("}")}
+  ),
+
+  s(
+    {trig = "wt"},
+    {t("\\widetilde{"), i(1), t("}")}
+  ),
+
+  s(
+    {trig = "bb"},
+    {t("\\mathbb{"), i(1), t("}")}
+  ),
+
+  s(
+    {trig = "bf"},
+    {t("\\mathbf{"), i(1), t("}")}
+  ),
+
+  s(
+    {trig = "cal"},
+    {t("\\mathcal{"), i(1), t("}")}
+  ),
+
+  s(
+    {trig = "scr"},
+    {t("\\mathscr{"), i(1), t("}")}
+  ),
+
+  s(
+    {trig = "frak"},
+    {t("\\mathfrak{"), i(1), t("}")}
+  ),
+
+  s(
+    {trig = "rm"},
+    {t("\\mathrm{"), i(1), t("}")}
+  ),
+
+  s(
+    {trig = "sf"},
+    {t("\\mathsf{"), i(1), t("}")}
+  ),
+
+  ms(
+    {{trig = "frac"}, {trig = "fr"}},
+    {t("\\frac{"), i(1), t("}{"), i(2), t("} ")}
+  ),
+
+  s(
+    {trig = "ovs"},
+    {t("\\overset{"), i(1), t("}{"), i(2), t("} ")}
+  ),
+
+  s(
+    {trig = "uns"},
+    {t("\\underset{"), i(1), t("}{"), i(2), t("} ")}
+  ),
+
+  s(
+    {trig = "ovl"},
+    {t("\\overline{"), i(1), t("}")}
+  ),
+
+  s(
+    {trig = "unl"},
+    {t("\\underline{"), i(1), t("}")}
+  ),
+
+  s(
+    {trig = "uns"},
+    {t("\\underset{"), i(1), t("}{"), i(2), t("} ")}
+  ),
+
+  s(
+    {trig = "tsc"},
+    {t("\\textsuperscript{"), i(1), t("}")}
+  ),
+
+  s(
+    {trig = "tbf"},
+    {t("\\textbf{"), i(1), t("}")}
+  ),
+
+  s(
+    {trig = "=="},
+    {t({"=", ""}), t("& \\, ")}
+  ),
+
+  ms(
+    {{trig = "beg"}, {trig = "begin"}},
+    {
+      t("\\begin{"), i(1), t({"}", ""}),
+      i(0),
+      t({"", "\\end{"}),
+      f(
+        function(args, _)
+          return args[1][1]
+        end,
+        {1},
+        {}
+      ),
+      t("}")
+    }
+  ),
+
+  ms(
+    {{trig = "item"}, {trig = "itemize"}},
+    {t({"\\begin{itemize}", "  \\item "}), i(0), t({"", "\\end{itemize}"})}
+  ),
+
+  s({trig = "it"}, {t({"", "\\item "})}),
+
+  ms(
+    {{trig = "enum"}, {trig = "enumerate"}},
+    {t({"\\begin{enumerate}", "  \\item "}), i(0), t({"", "\\end{enumerate}"})}
+  ),
+
+  s(
+    {trig = "ilm"},
+    {t("\\("), i(0), t("\\)")}
+  ),
+
+  s(
+    {trig = "eq"},
+    {t({"\\begin{equation}", "  "}), i(0), t({"", "\\end{equation}"})}
+  ),
+
+  s(
+    {trig = "eqnn"},
+    {t({"\\begin{equation*}", "  "}), i(0), t({"", "\\end{equation*}"})}
+  ),
+
+  s(
+    {trig = "al"},
+    {t({"\\begin{align}", "  "}), i(0), t({"", "\\end{align}"})}
+  ),
+
+  s(
+    {trig = "alnn"},
+    {t({"\\begin{align*}", "  "}), i(0), t({"", "\\end{align*}"})}
+  ),
+
+  s(
+    {trig = "spl"},
+    {
+      isn(
+        1,
+        {
+          t({"\\begin{split}", "  "}),
+          i(1),
+          t({"", "\\end{split}"})
+        },
+        "$PARENT_INDENT")
+    }
+  ),
+
+  -- TODO: Figure out how to implement snippetnode for title and label.
+  s(
+    {trig = "thm"},
+    {
+      t("\\begin{theorem}"),
+      sn(
+        1,
+        {t("["), i(1, "Name"), t({"]", "\\label{thm--"}), i(2), t({"}", ""})}
+      ),
+      i(0),
+      t({"", "\\end{theorem}"})
+    }
+  ),
+
+  s(
+    {trig = "prf"},
+    {
+      t({"\\begin{proof}", ""}),
+      i(0),
+      t({"", "\\end{proof}"})
+    }
+  ),
+
+  s(
+    {trig = "lem"},
+    {
+      t("\\begin{lemma}"),
+      sn(
+        1,
+        {t("["), i(1, "Name"), t({"]", "\\label{lem--"}), i(2), t({"}", ""})}
+      ),
+      i(0),
+      t({"", "\\end{lemma}"})
+    }
+  ),
+
+  s(
+    {trig = "cor"},
+    {
+      t("\\begin{corollary}"),
+      sn(
+        1,
+        {t("["), i(1, "Name"), t({"]", "\\label{cor--"}), i(2), t({"}", ""})}
+      ),
+      i(0),
+      t({"", "\\end{corollary}"})
+    }
+  ),
+
+  s(
+    {trig = "def"},
+    {
+      t("\\begin{definition}"),
+      sn(
+        1,
+        {t("["), i(1, "Name"), t({"]", "\\label{def--"}), i(2), t({"}", ""})}
+      ),
+      i(0),
+      t({"", "\\end{definition}"})
+    }
+  ),
+
+  s(
+    {trig = "asm"},
+    {
+      t("\\begin{assumption}"),
+      sn(
+        1,
+        {t("["), i(1, "Name"), t({"]", "\\label{asm--"}), i(2), t({"}", ""})}
+      ),
+      i(0),
+      t({"", "\\end{assumption}"})
+    }
+  ),
+
+  s(
+    {trig = "rem"},
+    {
+      t("\\begin{remark}"),
+      sn(
+        1,
+        {t("["), i(1, "Name"), t({"]", "\\label{rem--"}), i(2), t({"}", ""})}
+      ),
+      i(0),
+      t({"", "\\end{remark}"})
+    }
+  ),
+
+  s(
+    {trig = "eg"},
+    {
+      t("\\begin{example}"),
+      sn(
+        1,
+        {t("["), i(1, "Name"), t({"]", "\\label{eg--"}), i(2), t({"}", ""})}
+      ),
+      i(0),
+      t({"", "\\end{example}"})
+    }
+  ),
+
+  -- Frames for beamer
+  s(
+    {trig = "frame"},
+    {
+      t({"\\begin{frame}", ""}),
+      sn(
+        1,
+        {t("\\frametitle{"), i(1), t({"}", ""})}
+      ),
+      i(0),
+      t({"", "\\end{frame}"})
+    }
+  ),
+
+  s(
+    {trig = "itfr"},
+    {
+      t({"\\begin{frame}", ""}),
+      sn(
+        1,
+        {t("\\frametitle{"), i(1), t({"}", ""})}
+      ),
+      t({"\\begin{itemize}", ""}),
+      t("  \\item "), i(0),
+      t({"", "\\end{itemize}", ""}),
+      t("\\end{frame}")
+    }
+  ),
+
+  -- Left/right pairs
+  s(
+    {trig = "\\left(", snippetType = "autosnippet"},
+    -- {t("\\left( "), i(0), t(" \\right)")}
+    {t("\\left( "), i(0), t(" \\right"), i(1)}
+  ),
+
+  s(
+    {trig = "\\left[", snippetType = "autosnippet"},
+    -- {t("\\left[ "), i(0), t(" \\right]")}
+    {t("\\left[ "), i(0), t(" \\right"), i(1)}
+  ),
+
+  s(
+    {trig = "\\left\\{", snippetType = "autosnippet"},
+    {t("\\left\\{ "), i(0), t(" \\right\\}")}
+  ),
+
+  s(
+    {trig = "\\left|", snippetType = "autosnippet"},
+    {t("\\left| "), i(0), t(" \\right|")}
+  ),
+
+  s(
+    {trig = "\\left\\|", snippetType = "autosnippet"},
+    {t("\\left\\| "), i(0), t(" \\right\\|")}
+  ),
+
+}
---
+++ nvim/init.lua
@@ -0,0 +1,154 @@
+--------------------------------------------------------------------------------
+--- Settings
+--------------------------------------------------------------------------------
+local opt = vim.opt  -- for conciseness
+
+-- Line numbers, make them relative.
+opt.number = true
+opt.relativenumber = true
+
+-- Adjust splitting behavior
+opt.splitbelow = true
+opt.splitright = true
+
+-- Set highlight on search
+opt.hlsearch = false
+
+-- Case-insensitive searching UNLESS \C or capital in search
+opt.incsearch  = true
+opt.ignorecase = true
+opt.smartcase  = true
+
+-- Tabs and indentation
+opt.cindent     = false
+opt.autoindent  = true
+opt.smartindent = true
+opt.tabstop     = 2
+opt.softtabstop = 2
+opt.shiftwidth  = 2
+opt.expandtab   = true
+
+-- Enable mouse mode
+opt.mouse = 'a'
+
+-- Sync clipboard between OS and Neovim. See `:help 'clipboard'`
+opt.clipboard = 'unnamedplus'
+
+-- Save undo history
+opt.undofile = true
+
+-- Keep signcolumn on by default
+opt.signcolumn = 'yes'
+
+-- Enable break indent
+opt.breakindent = true
+
+-- Break lines at
+opt.linebreak = true
+
+-- Column indicators
+opt.colorcolumn = '81,101,121'
+
+-- Highlight current line
+opt.cursorline = true
+
+-- Color theme
+opt.background = "dark"
+
+-- Disable vim folds
+opt.foldenable = false
+
+-- Some globals
+vim.g.python3_host_prog   = '~/miniforge3/bin/python3'
+vim.g.tex_flavor          = 'latex'
+vim.g.tex_comment_nospell = 1
+-- vim.g.tex_nospell      = 1
+
+vim.cmd('set iskeyword-=_')
+
+-- Some settings to reorganize.
+--[[
+opt.updatetime = 1000
+opt.shortmess = opt.shortmess .. 'FcI'
+--]]
+
+--------------------------------------------------------------------------------
+--- Basic keymaps
+--------------------------------------------------------------------------------
+vim.g.mapleader = " "
+vim.g.maplocalleader = " m"
+
+local keyopts = {silent = true, noremap = true}
+local keymap = vim.keymap.set
+
+keymap({"n", "v"}, "j", "gj", keyopts)
+keymap({"n", "v"}, "k", "gk", keyopts)
+keymap("v", "<", "<gv", keyopts)                   -- Better indent in visual
+keymap("v", ">", ">gv", keyopts)                   -- Better dedent in visual
+keymap({"i", "v"}, "fd", "<esc>", keyopts)         -- fd = esc in insert
+keymap("t", "<c-f><c-d>", "<c-\\><c-n>", keyopts)  -- <c-f><c-d> = esc in term
+keymap("i", "<m-b>", "<c-left>", keyopts)
+keymap("i", "<m-f>", "<c-right>", keyopts)
+keymap("i", "<c-b>", "<left>", keyopts)
+keymap("i", "<c-f>", "<right>", keyopts)
+keymap({"n", "v"}, "<leader>fs", ":update<cr>", keyopts)
+
+keyopts.desc = "+window"
+keymap("n", "<leader>w", "<c-w>", keyopts)
+
+keyopts.desc = "delete"
+keymap('n', '<c-w>x', ':<c-u>bdel<cr>', keyopts)
+keymap('n', '<leader>wx', ':<c-u>bdel<cr>', keyopts)
+
+keyopts.desc = 'Copy Absolute Path'
+keymap('n', '<leader>fyy', ':<c-u>let @+=expand("%:p")<cr>', keyopts)
+
+keyopts.desc = 'Copy Directory Path'
+keymap('n', '<leader>fyd', ':<c-u>let @+=expand("%:p:h")<cr>', keyopts)
+
+keyopts.desc = 'Copy File Name'
+keymap('n', '<leader>fyn', ':<c-u>let @+=expand("%:t")<cr>', keyopts)
+
+keyopts.desc = 'Copy Relative File Name'
+keymap('n', '<leader>fyr', ':<c-u>let @+=expand("%")<cr>', keyopts)
+
+keyopts.desc = 'Delete Buffer'
+keymap('n', '<leader>bd', ':<c-u>bprev<cr>:bdel #<cr>', keyopts)
+
+keyopts.desc = 'Previous Buffer'
+keymap('n', '<leader>bp', ':<c-u>bprev<cr>', keyopts)
+keymap('n', '<leader>b[', ':<c-u>bprev<cr>', keyopts)
+
+keyopts.desc = 'Next Buffer'
+keymap('n', '<leader>bn', ':<c-u>bnext<cr>', keyopts)
+keymap('n', '<leader>b]', ':<c-u>bnext<cr>', keyopts)
+
+keyopts.desc = 'Clear search'
+keymap('n', '<leader>sc', ':nohl<cr>', keyopts)
+
+keyopts.desc = 'Terminal Other Window'
+keymap('n', '<leader>at', ':<c-u>vsp | term<cr>', keyopts)
+
+keyopts.desc = 'Terminal Current Window'
+keymap('n', '<leader>aT', ':<c-u>term<cr>', keyopts)
+
+--------------------------------------------------------------------------------
+--- Packages
+--------------------------------------------------------------------------------
+vim.pack.add({
+
+  -- Colorscheme
+  {src = "https://github.com/ellisonleao/gruvbox.nvim"},
+
+  -- Editor
+  -- {src = "https://github.com/lewis6991/spaceless.nvim"},
+
+  -- File browsing
+  {src = "https://github.com/stevearc/oil.nvim"}
+})
+
+
+--------------------------------------------------------------------------------
+--- Oil configuration
+--------------------------------------------------------------------------------
+require("oil").setup()
