cec035c748e989eb96371932889ebae9fa8b10a7
Ahnaf Rafi <ahnafrafi@ECON-DYR5QNPF6C.local>
2025-09-19 00:52:28 -0400

    Add neovim-nightly overlay

--- flake.nix
+++ flake.nix
@@ -17,6 +17,9 @@
     nix-homebrew.url = "github:zhaofengli/nix-homebrew";
     homebrew-core = { url = "github:homebrew/homebrew-core"; flake = false; };
     homebrew-cask = { url = "github:homebrew/homebrew-cask"; flake = false; };
+
+    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+
   };
 
   outputs = {
